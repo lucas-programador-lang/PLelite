80a5c019e41f061367b2ef7a7cf3b22bf12fc502
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-08-26 23:46:45 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -15,15 +15,14 @@ import {
 
 // SUAS CONFIGURAÇÕES DO FIREBASE AQUI
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyA6LJUVIThMhRl87W10-A-wKRneohDYrJU",
+  authDomain: "plelite.firebaseapp.com",
+  databaseURL: "https://plelite-default-rtdb.firebaseio.com",
+  projectId: "plelite",
+  storageBucket: "plelite.firebasestorage.app",
+  messagingSenderId: "561668288888",
+  appId: "1:561668288888:web:77c9406f54c8d32255188d"
 };
-
 // Inicialização
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
